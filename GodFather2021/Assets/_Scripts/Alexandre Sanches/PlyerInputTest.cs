0bbc6b82d39c2fa20d5eee291e6454f4e2578613
Sanches Mateus Alexandre <alexandre.sanches_mateus@edu.devinci.fr>
2021-09-08 17:00:47 +0200

    Finition enemy système

---
+++ GodFather2021/Assets/_Scripts/Alexandre Sanches/PlyerInputTest.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlyerInputTest : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit rayInfo;
+
+        Physics.Raycast(transform.position, Vector3.right, out rayInfo);
+
+        Debug.DrawRay(transform.position, Vector3.right * 10);
+    }
+}
---
+++ GodFather2021/Assets/_Scripts/Alexandre Sanches/WaveManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class WaveManager
+{
+    [Header ("Général")]
+    [TextArea (3,5)]
+    public string description;
+    public float timeBeforeWaveStart = 2f;
+
+    [Header ("Avanced")]
+    public int numberMime;
+    public float timeBetweenMime;
+    public int spawnPosition = 0;
+    [Range (0,10)]
+    public float propChangeLetter = 0;
+    public bool haveSameLetter;
+}
