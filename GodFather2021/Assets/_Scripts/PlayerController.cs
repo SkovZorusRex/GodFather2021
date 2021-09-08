75e77cdd2677f13b7adcd4b0257cb24ede99bd37
Paul <paul.putegnat@edu.devinci.fr>
2021-09-08 15:43:20 +0200

    Modif PlayerController

--- GodFather2021/Assets/_Scripts/PlayerController.cs
+++ GodFather2021/Assets/_Scripts/PlayerController.cs
@@ -6,9 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     private bool isMoving;
     private bool damaged;
-    public GameManager gameManager;
     private Vector3 originalPos, targetPos;
-    private Vector3 up = new Vector3(0,3.5f,0);
+    private Vector3 up = new Vector3(0, 3.5f, 0);
     private Vector3 down = new Vector3(0, -3.5f, 0);
     private Vector3 left = new Vector3(-5.5f, 0, 0);
     private Vector3 right = new Vector3(5.5f, 0, 0);
@@ -22,7 +21,11 @@ public class PlayerController : MonoBehaviour
             StartCoroutine(MovePlayer(down, 0.2f));
         if (Input.GetKey(KeyCode.A) && !isMoving && !damaged)
             Damaged();
-        //else gameManager.GameOver();
+        else if (Input.GetKey(KeyCode.A) && damaged && !isMoving)
+        {
+            StartCoroutine(MovePlayer(left, 0.2f));
+            Invoke("SecondDamage", 0.5f);
+        }
     }
 
     void Damaged()
@@ -30,12 +33,18 @@ public class PlayerController : MonoBehaviour
         damaged = true;
         StartCoroutine(MovePlayer(left, 0.2f));
         Invoke("Recup", 3);
+
     }
 
     void Recup()
     {
         StartCoroutine(MovePlayer(right, 0.6f));
         damaged = false;
+    }
+
+    void SecondDamage()
+    {
+        GameManager.instance.GameOver();
     }
 
     private IEnumerator MovePlayer(Vector3 direction, float timeToMove)
@@ -47,11 +56,11 @@ public class PlayerController : MonoBehaviour
         originalPos = transform.position;
         targetPos = originalPos + direction;
 
-        while(elapsedTime < timeToMove)
+        while (elapsedTime < timeToMove)
         {
             transform.position = Vector3.Lerp(originalPos, targetPos, (elapsedTime / timeToMove));
             elapsedTime += Time.deltaTime;
-            yield return null;
+            yield return null; 
         }
 
         transform.position = targetPos;
