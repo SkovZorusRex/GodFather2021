2cae1ca4fe643bb42ce08e7395345eed12549849
Sanches Mateus Alexandre <alexandre.sanches_mateus@edu.devinci.fr>
2021-09-09 09:24:44 +0200

    Fonction de mort Mime

--- GodFather2021/Assets/_Scripts/Alexandre Sanches/MimeMouv.cs
+++ GodFather2021/Assets/_Scripts/Alexandre Sanches/MimeMouv.cs
@@ -5,22 +5,33 @@ using UnityEngine.UI;
 
 public class MimeMouv : MonoBehaviour
 {
-    [Header ("Déplacement")]
+    [Header ("Mouvement")]
     public float speed = 5;
 
     private Vector3 endRun;
+    private Rigidbody2D rb;
+    private Collider2D colliderMime;
 
-
-    [Header ("Affichage lettre")]
+    [Header ("Display")]
     public float wait = 1f;
     public Text uiLetters;
 
     public string newLetter;
     public string letter;
 
+    [Header("Death Settings")]
+    public float maxUpForce;
+    public float minUpForce;
+    public float maxAngle;
+    public float maxTorque;
+    public float minTorque;
+
 
     private void Start()
     {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        colliderMime = gameObject.GetComponent<Collider2D>();
+
         // Remise à zero de l'UI
         uiLetters.text = "";
 
@@ -37,6 +48,10 @@ public class MimeMouv : MonoBehaviour
         float step = speed * Time.deltaTime;
         transform.position = Vector3.MoveTowards(transform.position, endRun, step);
 
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Death();
+        }
     }
 
 
@@ -54,5 +69,16 @@ public class MimeMouv : MonoBehaviour
         // Destruction du gomeObject après un certain temps
         yield return new WaitForSeconds(5);
         Destroy(gameObject);
+    }
+
+    public void Death()
+    {
+        Debug.Log("ouiiiiiiiiiii");
+        colliderMime.enabled = false;
+        rb.gravityScale = 1;
+        rb.drag = 1;
+
+        rb.AddForce(new Vector2(Random.Range(minUpForce,maxUpForce), Random.Range(-maxAngle,maxAngle)));
+        rb.AddTorque(Random.Range(maxTorque, minTorque));
     }
 }
